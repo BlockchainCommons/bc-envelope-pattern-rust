0d9fd3488de098dd59ebdf69e780cc9f47d1d7ad
Wolf McNally <wolfmcnally@users.noreply.github.com>
2025-06-18 01:39:33 -0700

    Add parsers for node, wrapped, and subject patterns

--- src/parse/structure/mod.rs
+++ src/parse/structure/mod.rs
@@ -1,3 +1,54 @@
 // Parsers for structure-level pattern syntax
 
-// Currently unused but reserved for future expansion.
+use super::{meta, Token};
+use crate::{Error, Pattern, Result};
+
+pub(crate) fn parse_node(lexer: &mut logos::Lexer<Token>) -> Result<Pattern> {
+    let mut lookahead = lexer.clone();
+    match lookahead.next() {
+        Some(Ok(Token::ParenOpen)) => {
+            lexer.next();
+            match lexer.next() {
+                Some(Ok(Token::Range(res))) => {
+                    let range = res?;
+                    let pat = if let Some(max) = range.max() {
+                        Pattern::node_with_assertions_range(range.min()..=max)
+                    } else {
+                        Pattern::node_with_assertions_range(range.min()..)
+                    };
+                    match lexer.next() {
+                        Some(Ok(Token::ParenClose)) => Ok(pat),
+                        Some(Ok(t)) => Err(Error::UnexpectedToken(Box::new(t), lexer.span())),
+                        Some(Err(e)) => Err(e),
+                        None => Err(Error::ExpectedCloseParen(lexer.span())),
+                    }
+                }
+                Some(Ok(t)) => Err(Error::UnexpectedToken(Box::new(t), lexer.span())),
+                Some(Err(e)) => Err(e),
+                None => Err(Error::UnexpectedEndOfInput),
+            }
+        }
+        _ => Ok(Pattern::any_node()),
+    }
+}
+
+pub(crate) fn parse_wrapped(_lexer: &mut logos::Lexer<Token>) -> Result<Pattern> {
+    Ok(Pattern::wrapped())
+}
+
+pub(crate) fn parse_subject(lexer: &mut logos::Lexer<Token>) -> Result<Pattern> {
+    let mut lookahead = lexer.clone();
+    match lookahead.next() {
+        Some(Ok(Token::ParenOpen)) => {
+            lexer.next();
+            let pattern = meta::parse_or(lexer)?;
+            match lexer.next() {
+                Some(Ok(Token::ParenClose)) => Ok(Pattern::subject(pattern)),
+                Some(Ok(t)) => Err(Error::UnexpectedToken(Box::new(t), lexer.span())),
+                Some(Err(e)) => Err(e),
+                None => Err(Error::ExpectedCloseParen(lexer.span())),
+            }
+        }
+        _ => Ok(Pattern::any_subject()),
+    }
+}
