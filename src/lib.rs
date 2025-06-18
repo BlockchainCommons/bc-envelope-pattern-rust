d3b7a8f4b204f498a9b7cfea7810060e26187c63
Wolf McNally <wolf@wolfmcnally.com>
2025-06-17 19:24:06 -0700

    Working.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod parse;
 mod pattern;
-mod repeat;
+mod interval;
 mod reluctance;
+mod quantifier;
 
 pub use parse::{
-    Error as ParseError, Result as ParseResult, Token, parse_pattern,
+    Error as ParseError, Result as ParseResult, parse_pattern,
 };
 pub use pattern::{Matcher, Path, Pattern};
-pub use repeat::{Interval, Quantifier};
+pub use interval::Interval;
 pub use reluctance::Reluctance;
+pub use quantifier::Quantifier;
---
+++ src/quantifier.rs
@@ -0,0 +1,65 @@
+use std::ops::RangeBounds;
+
+use crate::{Interval, Reluctance};
+
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Quantifier {
+    interval: Interval,
+    reluctance: Reluctance,
+}
+
+impl Quantifier {
+    pub fn new(
+        interval: impl RangeBounds<usize>,
+        reluctance: Reluctance,
+    ) -> Self {
+        let repeat = Interval::new(interval);
+        Self { interval: repeat, reluctance }
+    }
+
+    pub fn min(&self) -> usize { self.interval.min() }
+
+    pub fn max(&self) -> Option<usize> { self.interval.max() }
+
+    pub fn reluctance(&self) -> Reluctance { self.reluctance }
+
+    pub fn contains(&self, count: usize) -> bool {
+        self.interval.contains(count)
+    }
+
+    pub fn is_unbounded(&self) -> bool { self.interval.is_unbounded() }
+}
+
+impl std::fmt::Display for Quantifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}",
+            self.interval.shorthand_notation(),
+            self.reluctance.suffix()
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_quantifier_display() {
+        assert_eq!(format!("{}", Quantifier::new(1..=5, Reluctance::Greedy)), "{1,5}");
+        assert_eq!(format!("{}", Quantifier::new(3..=3, Reluctance::Lazy)), "{3}?");
+        assert_eq!(format!("{}", Quantifier::new(2.., Reluctance::Possessive)), "{2,}+");
+        assert_eq!(format!("{}", Quantifier::new(0.., Reluctance::Greedy)), "*");
+        assert_eq!(format!("{}", Quantifier::new(0.., Reluctance::Lazy)), "*?");
+        assert_eq!(format!("{}", Quantifier::new(0.., Reluctance::Possessive)), "*+");
+        assert_eq!(format!("{}", Quantifier::new(1.., Reluctance::Greedy)), "+");
+        assert_eq!(format!("{}", Quantifier::new(1.., Reluctance::Lazy)), "+?");
+        assert_eq!(format!("{}", Quantifier::new(1.., Reluctance::Possessive)), "++");
+        assert_eq!(format!("{}", Quantifier::new(0..=1, Reluctance::Greedy)), "?");
+        assert_eq!(format!("{}", Quantifier::new(0..=1, Reluctance::Lazy)), "??");
+        assert_eq!(format!("{}", Quantifier::new(0..=1, Reluctance::Possessive)), "?+");
+    }
+}
